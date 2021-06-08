c2a5269de708aa1e72e074ad1f0f15bab373c7e5
Marty Kokes <marty.kokes@luminfire.com>
2021-06-08 16:11:30 -0500

    Adjusted function binding
    Adjusted function binding

--- src/index.js
+++ src/index.js
@@ -5,18 +5,13 @@ class UppyBrilliantStorage extends Plugin {
     super(uppy, opts)
     this.id = this.opts.id || 'BrilliantStorage'
     this.type = 'modifier'
+
     this.getUploadParameters = this.getUploadParameters.bind(this)
-
-
-    // we use those internally in `this.compress`, so they
-    // should not be overriden
-    delete this.opts.success
-    delete this.opts.error
-
     this.prepareUpload = this.prepareUpload.bind(this)
-    this.compress = this.compress.bind(this)
+
   }
 
+  // Brilliant Storage Settings.
   getUploadParameters (file) {
     console.log(this.opts)
     console.log(file)
@@ -38,49 +33,33 @@ class UppyBrilliantStorage extends Plugin {
       .then(assertServerError)
   }
 
-  // TODO: replace with file prefix handling
-  // prepareUpload (fileIDs) {
-  //   const promises = fileIDs.map((fileID) => {
-  //     const file = this.uppy.getFile(fileID)
-  //     this.uppy.emit('preprocess-progress', file, {
-  //       mode: 'indeterminate',
-  //       message: this.i18n('compressingImages')
-  //     })
+  // Prefix Handling.
+  prepareUpload (fileIDs) {
+    const promises = fileIDs.map((fileID) => {
+      const file = this.uppy.getFile(fileID)
+      this.uppy.emit('preprocess-progress', file, {
+        mode: 'indeterminate',
+        message: this.i18n('addingPrefix')
+      })
 
-  //     if (file.type.split('/')[0] !== 'image') {
-  //       return
-  //     }
-
-  //     return this.compress(file.data).then((compressedBlob) => {
-  //       this.uppy.log(`[Image Compressor] Image ${file.id} size before/after compression: ${file.data.size} / ${compressedBlob.size}`)
-  //       this.uppy.setFileState(fileID, { data: compressedBlob })
-  //     }).catch((err) => {
-  //       this.uppy.log(`[Image Compressor] Failed to compress ${file.id}:`, 'warning')
-  //       this.uppy.log(err, 'warning')
-  //     })
-  //   })
-
-  //   const emitPreprocessCompleteForAll = () => {
-  //     fileIDs.forEach((fileID) => {
-  //       const file = this.uppy.getFile(fileID)
-  //       this.uppy.emit('preprocess-complete', file)
-  //     })
-  //   }
-
-  //   // Why emit `preprocess-complete` for all files at once, instead of
-  //   // above when each is processed?
-  //   // Because it leads to StatusBar showing a weird “upload 6 files” button,
-  //   // while waiting for all the files to complete pre-processing.
-  //   return Promise.all(promises)
-  //     .then(emitPreprocessCompleteForAll)
-  // }
+      return 
+      // return this.compress(file.data).then((compressedBlob) => {
+      //   this.uppy.log(`[Image Compressor] Image ${file.id} size before/after compression: ${file.data.size} / ${compressedBlob.size}`)
+      //   this.uppy.setFileState(fileID, { data: compressedBlob })
+      // }).catch((err) => {
+      //   this.uppy.log(`[Image Compressor] Failed to compress ${file.id}:`, 'warning')
+      //   this.uppy.log(err, 'warning')
+      // })
+    })
 
   install () {
     this.uppy.addPreProcessor(this.getUploadParameters)
+    this.uppy.addPreProcessor(this.prepareUpload)
   }
 
   uninstall () {
     this.uppy.removePreProcessor(this.getUploadParameters)
+    this.uppy.removePreProcessor(this.prepareUpload)
   }
 }
 
